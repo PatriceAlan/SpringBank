9dbcb0d14fed0e2e59f0a0bc668947143db34bad
PatriceAlan <peckpatricealan@gmail.com>
2023-12-09 22:58:27 +0100

    Fichiers repository

---
+++ src/main/java/com/project/SpringBank/repositories/ClientRepository.java
@@ -0,0 +1,10 @@
+package com.project.SpringBank.repositories;
+
+import com.project.SpringBank.entities.Client;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends CrudRepository<Client, Long> {
+
+}
