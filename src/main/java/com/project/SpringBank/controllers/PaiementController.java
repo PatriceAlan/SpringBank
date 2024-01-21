d1d332bc3f7bc7bf63dfd40fdb42818ed6c39a72
PatriceAlan <peckpatricealan@gmail.com>
2024-01-21 21:37:25 +0100

    Modifications in services and modifications in controllers...

--- src/main/java/com/project/SpringBank/controllers/PaiementController.java
+++ src/main/java/com/project/SpringBank/controllers/PaiementController.java
@@ -1,4 +1,53 @@
 package com.project.SpringBank.controllers;
 
+import com.project.SpringBank.entities.Paiement;
+import com.project.SpringBank.services.paiement.PaiementDto;
+import com.project.SpringBank.services.paiement.PaiementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/paiements")
 public class PaiementController {
+
+    private final PaiementService paiementService;
+
+    @Autowired
+    public PaiementController(PaiementService paiementService) {
+        this.paiementService = paiementService;
+    }
+
+    @PostMapping("/ajouter-paiement")
+    public ResponseEntity<Paiement> ajouterOuMettreAJourPaiement(@RequestBody Paiement paiement) {
+        try {
+            Paiement savedPaiement = paiementService.sauvegarderOuMettreAJourPaiement(paiement);
+            return new ResponseEntity<>(savedPaiement, HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Paiement> rechercherPaiementParId(@PathVariable Long id) {
+        try {
+            Paiement paiement = paiementService.rechercherPaiement(id);
+            return paiement != null
+                    ? new ResponseEntity<>(paiement, HttpStatus.OK)
+                    : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> supprimerPaiement(@PathVariable Long id) {
+        try {
+            paiementService.supprimerPaiement(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
 }
--- src/main/java/com/project/SpringBank/controllers/VirementController.java
+++ src/main/java/com/project/SpringBank/controllers/VirementController.java
@@ -1,4 +1,74 @@
 package com.project.SpringBank.controllers;
 
+import com.project.SpringBank.entities.Virement;
+import com.project.SpringBank.services.virement.VirementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/virements")
 public class VirementController {
+
+    private final VirementService virementService;
+
+    @Autowired
+    public VirementController(VirementService virementService) {
+        this.virementService = virementService;
+    }
+
+    @PostMapping("/ajouter-virement")
+    public ResponseEntity<Virement> ajouterOuMettreAJourVirement(@RequestBody Virement virement) {
+        try {
+            Virement savedVirement = virementService.sauvegarderOuMettreAJourVirement(virement);
+            return new ResponseEntity<>(savedVirement, HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Virement> rechercherVirementParId(@PathVariable Long id) {
+        try {
+            Virement virement = virementService.rechercherVirement(id);
+            return virement != null
+                    ? new ResponseEntity<>(virement, HttpStatus.OK)
+                    : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/virements-emis/{iban}")
+    public ResponseEntity<List<Virement>> listerVirementsEmisDuCompte(@PathVariable String iban) {
+        try {
+            List<Virement> virementsEmis = virementService.listerVirementsEmisDuCompte(iban);
+            return new ResponseEntity<>(virementsEmis, HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/virements-recus/{iban}")
+    public ResponseEntity<List<Virement>> listerVirementsRecusDuCompte(@PathVariable String iban) {
+        try {
+            List<Virement> virementsRecus = virementService.listerVirementsRecusSurLeCompte(iban);
+            return new ResponseEntity<>(virementsRecus, HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/tous-les-virements-du-compte/{iban}")
+    public ResponseEntity<List<Virement>> listerTousLesVirementsDuCompte(@PathVariable String iban) {
+        try {
+            List<Virement> tousLesVirements = virementService.listerTousLesVirementsDuCompte(iban);
+            return new ResponseEntity<>(tousLesVirements, HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
 }
