fe8584a5b24efbe6d2ffcb98c9e5942bc0f46010
PatriceAlan <peckpatricealan@gmail.com>
2025-11-13 23:08:32 +0100

    added module, routing, components in springbank frontend

---
+++ springbank-frontend/src/app/comptes/comptes.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ComptesRoutingModule } from './comptes-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ComptesRoutingModule
+  ]
+})
+export class ComptesModule { }
